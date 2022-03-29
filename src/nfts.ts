d8bb7c7da28984b7ab945fecf176069557521e7f
musnit <musnit@gmail.com>
2022-03-29 17:55:38 -0400

    fix dup track bug

--- src/nfts.ts
+++ src/nfts.ts
@@ -21,14 +21,16 @@ const getNFTMetadataCall = (nft: NFT) => {
 
 const filterExistingTrackNFTs = async (nfts: NFT[], dbClient: DBClient) => {
   let newTrackNFTs = [];
+  let newTrackIds: any = {};
   for (let i = 0; i < nfts.length; i++) {
     const nft = nfts[i];
     if (!nft || !nft.track) {
       console.log({ nft });
       throw new Error('Error processing NFT');
     }
-    const isExistingTrack = await dbClient.trackExists(nft.track.id)
+    const isExistingTrack = (await dbClient.trackExists(nft.track.id)) || newTrackIds[nft.track.id];
     if (!isExistingTrack) {
+      newTrackIds[nft.track.id] = true;
       newTrackNFTs.push(nft);
     }
   }
@@ -49,9 +51,5 @@ export const processTracksFromNFTs = async (nfts: NFT[], dbClient: DBClient) => 
   // todo: do bulk metadata call to get all metadata urls and add to track record
   // todo: do metadata url calls to get all metadata and add to track record
   // todo: postfilter/zora only music&catalog filter/extra noizd calls/extra centralized calls for optimized media
-  console.log({
-    l1: nfts.length,
-    l2: newTracks.length
-  })
   return newTracks.map(track => track.record);
 };
