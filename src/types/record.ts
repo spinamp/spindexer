c6006a975ea5ddfce33a15b673778ec1250c2878
musnit <musnit@gmail.com>
2022-04-15 11:29:22 -0400

    subgraph nft cleanup

--- src/types/record.ts
+++ src/types/record.ts
@@ -11,15 +11,3 @@ export type TimeField = EthereumBlockNumberField & DateTimeField;
 export type Record = {
   id: string
 } & TimeField
-
-export enum RecordType {
-  nft = "nft",
-  track = "track"
-}
-
-export const recordsEqual = (recordA: Record, recordB: Record) => recordA.id == recordB.id;
-
-export const recordIsInBatch = (record: Record, batch: Record[]) => {
-  const match = batch.find((batchRecord: Record) => recordsEqual(batchRecord, record));
-  return !!match;
-};
