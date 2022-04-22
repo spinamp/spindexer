768d16df0bafa9cf7e0d612c00e16f75aafe6714
musnit <musnit@gmail.com>
2022-04-21 21:42:51 -0400

    nft processor fixes

--- src/types/ethereum.ts
+++ src/types/ethereum.ts
@@ -11,14 +11,15 @@ export type ERC721Contract = {
   address: string,
   platform: MusicPlatform,
   startingBlock: string,
-  buildNFTMetadataId: (contractAddress: string, tokenId: BigInt) => string,
-  contractType: keyof NFTContractTypes,
+  contractType: ContractTypeName,
 }
 
 type NFTContractTypes = {
   [type:string] : {
     contractCalls: ValidContractCallFunction[],
     contractMetadataField: ValidContractCallFunction,
+    buildNFTId: (contractAddress: string, tokenId: BigInt) => string,
+    buildNFTMetadataId: (contractAddress: string, tokenId: BigInt) => string,
     }
 }
 
@@ -26,40 +27,45 @@ export const NFTContractTypes:NFTContractTypes = {
   default: {
     contractCalls: [ValidContractCallFunction.tokenURI],
     contractMetadataField: ValidContractCallFunction.tokenURI,
+    buildNFTId: buildERC721Id,
+    buildNFTMetadataId: buildERC721Id,
   },
   zora: {
     contractCalls: [ValidContractCallFunction.tokenURI, ValidContractCallFunction.tokenMetadataURI],
     contractMetadataField: ValidContractCallFunction.tokenMetadataURI,
+    buildNFTId: buildERC721Id,
+    buildNFTMetadataId: buildERC721Id,
   }
 }
 
-
 export function buildERC721Id(contractAddress: string, tokenId: BigInt): string {
   return `${formatAddress(contractAddress)}/${tokenId.toString()}`;
+}
+
+export enum ContractTypeName {
+  default = 'default',
+  zora = 'zora'
 }
 
 export const ZoraContract:ERC721Contract = {
   address: '0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7',
   platform: MusicPlatform.zora,
   startingBlock: '11565020',
-  buildNFTMetadataId: buildERC721Id,
-  contractType: 'zora',
+  contractType: ContractTypeName.zora,
 };
 
 export const NOIZDContract:ERC721Contract = {
   address: '0xf5819e27b9bad9f97c177bf007c1f96f26d91ca6',
   platform: MusicPlatform.noizd,
   startingBlock: '13470560',
-  buildNFTMetadataId: buildERC721Id,
-  contractType: 'default',
+  contractType: ContractTypeName.default,
 };
 
 export const NewCatalogContract:ERC721Contract = {
   address: '0x0bc2a24ce568dad89691116d5b34deb6c203f342',
   platform: MusicPlatform.catalog,
   startingBlock: '14566825',
-  buildNFTMetadataId: buildERC721Id,
-  contractType: 'default',
+  contractType: ContractTypeName.default,
 };
 
 export const ERC721_CONTRACTS = [
