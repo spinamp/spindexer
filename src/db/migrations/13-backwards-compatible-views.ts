481a6ad7e87c76acdc5162768aa7f5e11ec97e76
Jamie Gregory <jamiegregsa+geimaj@gmail.com>
2022-08-01 13:25:41 +0200

    add views to support old graphql queries

---
+++ src/db/migrations/13-backwards-compatible-views.ts
@@ -0,0 +1,33 @@
+
+import { Knex } from 'knex';
+
+export const up = async (knex: Knex) => {
+
+  // backwards compatible view for nfts table
+  await knex.raw(
+    `create view erc721nft as
+    select * from nfts
+    `
+  )
+
+  // backwards compatible view for nfts_processedTracks table
+  await knex.raw(
+    `create view "erc721nfts_processedTracks" as
+    select npt."nftId" as "erc721NftId", npt."processedTrackId" 
+    from "nfts_processedTracks" npt 
+    `
+  )
+
+  // add comment to add fake fk constraints to view for postgraphile relation generation
+  await knex.raw(
+    `comment on view "erc721nfts_processedTracks" is
+    E'@foreignKey ("erc721NftId") references erc721nft (id)|@foreignKey ("processedTrackId") references "processedTracks" (id)';
+    `
+  )
+
+}
+
+export const down = async (knex: Knex) => {
+  await knex.raw(`drop view erc721nft`);
+  await knex.raw(`drop view "erc721nfts_processedTracks"`);
+}
