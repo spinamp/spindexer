333d696db0b1e76a7e8dafbf25b180e4fd5aacd9
Jamie Gregory <jamiegregsa+geimaj@gmail.com>
2022-08-03 14:26:45 +0200

    improve query

--- src/processors/default/createERC721NFTsFromTransfersProcessor.ts
+++ src/processors/default/createERC721NFTsFromTransfersProcessor.ts
@@ -20,8 +20,6 @@ const processorFunction = (contracts: NftFactory[]) =>
     const newNFTs: Partial<NFT>[] = [];
     const updates: Partial<NFT>[] = [];
     const transfers: Partial<ERC721Transfer>[] = [];
-    const allNfts = await clients.db.getRecords<NFT>(Table.nfts);
-    const allNftIds = new Set(allNfts.map(nft => nft.id));
     items.forEach((item): Partial<NFT> | undefined => {
       const address = item.address;
       const contract = contractsByAddress[address];
@@ -34,17 +32,15 @@ const processorFunction = (contracts: NftFactory[]) =>
 
       const tokenId = BigInt((item.args!.tokenId as BigNumber).toString());
       const newMint = item.args!.from === ETHEREUM_NULL_ADDRESS;
-      if (allNftIds.has(contractType.buildNFTId(contract.address, tokenId))){
-        transfers.push({
-          id: `${CHAIN}/${item.blockNumber}/${item.logIndex}`,
-          contractAddress: formatAddress(contract.address),
-          from: item.args!.from,
-          to: item.args!.to,
-          tokenId,
-          createdAtEthereumBlockNumber: '' + item.blockNumber,
-          nftId: contractType.buildNFTId(contract.address, tokenId), 
-        });
-      }
+      transfers.push({
+        id: `${CHAIN}/${item.blockNumber}/${item.logIndex}`,
+        contractAddress: formatAddress(contract.address),
+        from: item.args!.from,
+        to: item.args!.to,
+        tokenId,
+        createdAtEthereumBlockNumber: '' + item.blockNumber,
+        nftId: contractType.buildNFTId(contract.address, tokenId), 
+      });
       if (!newMint) {
         updates.push({
           id: contractType.buildNFTId(contract.address, tokenId),
@@ -61,9 +57,15 @@ const processorFunction = (contracts: NftFactory[]) =>
         owner: item.args!.to
       });
     });
+    
     await clients.db.insert(Table.nfts, newNFTs.filter(n => !!n), { ignoreConflict: 'id' });
     await clients.db.update(Table.nfts, updates);
-    await clients.db.insert(Table.erc721Transfers, transfers);
+    
+    const transferNftIds = transfers.map(transfer => transfer.nftId);
+    const existingNfts = new Set((await clients.db.getRecords<NFT>(Table.nfts, [ ['whereIn', [ 'id', transferNftIds ]] ])).map(nft => nft.id));
+    const transfersForExistingNfts = transfers.filter(transfer => existingNfts.has(transfer.nftId!));
+
+    await clients.db.insert(Table.erc721Transfers, transfersForExistingNfts);
     await clients.db.updateProcessor(NAME, newCursor);
   };
 
