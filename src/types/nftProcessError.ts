6a72b660307e9000df00297f544fded02aadb672
Jamie Gregory <jamiegregsa+geimaj@gmail.com>
2022-08-15 23:10:31 +0200

    remove unused type

--- src/types/nftProcessError.ts
+++ src/types/nftProcessError.ts
@@ -2,7 +2,6 @@ export type NFTProcessError = {
   nftId: string;
   processError?: string;
   metadataError?: string;
-  ipfsMediaError?: string;
   numberOfRetries?: number;
   lastRetry?: Date;
 }
