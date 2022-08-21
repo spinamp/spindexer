aae47d84cfec4e2970607ea5c05f4a3049c54c63
musnit <musnit@gmail.com>
2022-08-21 12:04:41 +0300

    update var name

--- src/db/migrations/17-add-heds7.ts
+++ src/db/migrations/17-add-heds7.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 import { NftFactory, NFTContractTypeName, NFTStandard } from '../../types/nft';
 import { addNftFactory, removeNftFactory } from '../migration-helpers';
 
-const HEDS_6: NftFactory = {
+const NFT_FACTORY: NftFactory = {
   address: '0x20f2717f113d0b3815124876f3d72f8e1179341e',
   startingBlock: '15373455',
   platformId: 'heds',
@@ -12,9 +12,9 @@ const HEDS_6: NftFactory = {
 };
 
 export const up = async (knex: Knex) => {
-  await addNftFactory(knex, HEDS_6)
+  await addNftFactory(knex, NFT_FACTORY)
 }
 
 export const down = async (knex: Knex) => {
-  await removeNftFactory(knex, HEDS_6)
+  await removeNftFactory(knex, NFT_FACTORY)
 }
