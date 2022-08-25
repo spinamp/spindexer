74279446bb6b5db0bb36b10892987614664b8dcb
Jamie Gregory <jamiegregsa+geimaj@gmail.com>
2022-08-25 14:54:58 +0200

    fix migration

--- src/db/migrations/19-add-approved-entities.ts
+++ src/db/migrations/19-add-approved-entities.ts
@@ -6,15 +6,25 @@ import { Table } from '../db'
 export const up = async (knex: Knex) => {
 
   // add approved column
-  await knex.schema.alterTable(Table.metaFactories, table => {
-    table.boolean('autoApprove').defaultTo(false)
-  })
-  await knex.schema.alterTable(Table.nftFactories, table => {
-    table.boolean('autoApprove').defaultTo(false)
-  })
-  await knex.schema.alterTable(Table.nfts, table => {
-    table.boolean('approved').defaultTo(false)
-  })
+  const shouldAlterMetaFactories = !(await knex.schema.hasColumn(Table.metaFactories, 'autoApprove'))
+  const shouldAlterNftFactories = !(await knex.schema.hasColumn(Table.nftFactories, 'autoApprove'))
+  const shouldAlterNfts = !(await knex.schema.hasColumn(Table.nfts, 'approved'))
+
+  if (shouldAlterMetaFactories){
+    await knex.schema.alterTable(Table.metaFactories, table => {
+      table.boolean('autoApprove').defaultTo(false)
+    })
+  }
+  if (shouldAlterNftFactories){
+    await knex.schema.alterTable(Table.nftFactories, table => {
+      table.boolean('autoApprove').defaultTo(false)
+    })
+  }
+  if (shouldAlterNfts){
+    await knex.schema.alterTable(Table.nfts, table => {
+      table.boolean('approved').defaultTo(false)
+    })
+  }
 
   // set all existing entities to approved = true
   await knex(Table.metaFactories).update({ autoApprove: true })
