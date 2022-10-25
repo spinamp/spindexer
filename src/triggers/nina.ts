b6eb9f12162117ee2c8c628410d8cd953005052a
Jamie Gregory <jamiegregsa@gmail.com>
2022-10-25 22:28:52 +0200

    fix nina

--- src/triggers/nina.ts
+++ src/triggers/nina.ts
@@ -9,63 +9,74 @@ import { NFT } from '../types/nft';
 import { MusicPlatformType } from '../types/platform';
 import { Trigger } from '../types/trigger';
 
-export const newNinaContracts: Trigger<undefined> = async (clients) => {
-  const factory = (await clients.db.getRecords<MetaFactory>(Table.metaFactories, [
-    [
-      'where', ['platformId', MusicPlatformType.nina]
-    ]
-  ]))[0]
+export const newNinaContracts: Trigger<undefined> = async clients => {
+  const factory = (
+    await clients.db.getRecords<MetaFactory>(Table.metaFactories, [
+      ['where', ['platformId', MusicPlatformType.nina]],
+    ])
+  )[0];
 
   const endpoint = process.env.SOLANA_PROVIDER_ENDPOINT;
 
   if (!endpoint) {
-    throw 'No solana endpoint configured'
+    throw 'No solana endpoint configured';
   }
 
-  if (!factory){
-    return []
+  if (!factory) {
+    return [];
   }
 
   const connection = new web3.Connection(endpoint);
-  const provider = new AnchorProvider(connection, new Wallet(new Keypair()), {})
-  const nina = await Program.at(
-    factory.id,
-    provider,
-  )
+  const provider = new AnchorProvider(
+    connection,
+    new Wallet(new Keypair()),
+    {},
+  );
+  const nina = await Program.at(factory.id, provider);
 
   const metaplex = new MetaplexFoundation.Metaplex(connection);
 
   // Fetch all releases from Solana via Anchor
   const releases = await nina.account.release.all();
   const metadataAccounts = (
-    await metaplex.nfts()
-      .findAllByMintList(
-        releases.map(
-          release => release.account.releaseMint as web3.PublicKey
-        )
-      )
-  ).filter(x => x);
+    await metaplex
+      .nfts()
+      .findAllByMintList({
+        mints: releases.map(
+          release => release.account.releaseMint as web3.PublicKey,
+        ),
+      })
+      .run()
+  ).filter(x => x) as MetaplexFoundation.Metadata<MetaplexFoundation.JsonMetadata<string>>[];
 
-  const existingContracts = (await clients.db.getRecords<NFT>(Table.nfts, [
-    [
-      'where', ['platformId', MusicPlatformType.nina]
-    ]
-  ])).map(nft => nft.contractAddress)
+  const existingContracts = (
+    await clients.db.getRecords<NFT>(Table.nfts, [
+      ['where', ['platformId', MusicPlatformType.nina]],
+    ])
+  ).map(nft => nft.contractAddress);
 
-  const allMintAccounts = new Set(metadataAccounts.map(account => account!.mint.toBase58()));
+  const allMintAccounts = new Set(
+    metadataAccounts.map(account => account!.mintAddress!.toBase58()),
+  );
   const existingMintAccounts = new Set(existingContracts);
 
-  const releasesByMintAddress = _.keyBy(releases, release => (release.account.releaseMint as web3.PublicKey).toBase58())
+  const releasesByMintAddress = _.keyBy(releases, release =>
+    (release.account.releaseMint as web3.PublicKey).toBase58(),
+  );
 
-  const newMintAccounts = new Set(([...allMintAccounts].filter(mint => !existingMintAccounts.has(mint))));
+  const newMintAccounts = new Set(
+    [...allMintAccounts].filter(mint => !existingMintAccounts.has(mint)),
+  );
 
-  return metadataAccounts.filter(account => newMintAccounts.has(account!.mint.toBase58())).map(account => ({
-    metadataAccount: account,
-    release: releasesByMintAddress[account!.mint.toBase58()]
-  }))
+  return metadataAccounts
+    .filter(account => newMintAccounts.has(account!.mintAddress.toBase58()))
+    .map(account => ({
+      metadataAccount: account,
+      release: releasesByMintAddress[account!.mintAddress.toBase58()],
+    }));
 };
 
-export const missingCreatedAtTimeWithMetadataDate: Trigger<undefined> = async (clients) => {
+export const missingCreatedAtTimeWithMetadataDate: Trigger<undefined> = async clients => {
   const nftQuery = `
     select *
     from "${Table.nfts}"
@@ -74,6 +85,6 @@ export const missingCreatedAtTimeWithMetadataDate: Trigger<undefined> = async (c
     limit ${process.env.QUERY_TRIGGER_BATCH_SIZE}
 `;
 
-  const nfts = (await clients.db.rawSQL(nftQuery)).rows
+  const nfts = (await clients.db.rawSQL(nftQuery)).rows;
   return nfts;
 };
