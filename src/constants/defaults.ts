f1edb5e2175b5147dd0484ce0aa93533d2908947
puffo <puffo@users.noreply.github.com>
2022-10-31 12:26:47 +0200

    refactor: remove method for default admin address :fire:

--- src/constants/defaults.ts
+++
@@ -1,5 +0,0 @@
-import { ETHEREUM_NULL_ADDRESS } from '../types/ethereum'
-
-export const defaultAdminAddress = () => {
-  return process.env.DEFAULT_ADMIN_ADDRESS?.toLowerCase() || ETHEREUM_NULL_ADDRESS
-}
