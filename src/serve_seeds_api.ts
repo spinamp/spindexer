df1db80f91b842ad8f4bec9e0eb3b145ce5ddd60
puffo <puffo@users.noreply.github.com>
2022-11-09 16:39:10 +0200

    fix: close DB connection after server start :bug:

--- src/serve_seeds_api.ts
+++ src/serve_seeds_api.ts
@@ -2,17 +2,29 @@ import 'dotenv/config';
 
 import db from '../src/db/sql-db';
 
+import { DBClient } from './db/db';
 import { createSeedsAPIServer } from './seeds/server';
 
+let globalDBClient: DBClient;
+
+const getDBClient = async () => {
+  if (!globalDBClient) {
+    globalDBClient = await db.init();
+  }
+  return globalDBClient;
+}
+
 const serveSeedsAPI = async () => {
-  const dbClient = await db.init();
+  const dbClient = await getDBClient();
   createSeedsAPIServer(dbClient).listen(process.env.SEEDS_API_PORT, () => {
     console.log(`Server running on port ${process.env.SEEDS_API_PORT}`);
   });
 }
 
-process.on('SIGINT', () => {
+process.on('SIGINT', async () => {
   console.log('Exiting...');
+  const dbClient = await getDBClient();
+  dbClient.close();
   setTimeout(() => process.exit(), 0);
 });
 
