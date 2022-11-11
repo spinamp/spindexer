d7feaba8c46aba1994fe1957011119413bc00cac
puffo <puffo@users.noreply.github.com>
2022-11-11 18:27:15 +0200

    fix: adjust types to support new fields for artist :sparkles:

--- src/types/artist.ts
+++ src/types/artist.ts
@@ -4,6 +4,11 @@ import { Record, TimeField } from './record'
 
 export type Artist = Record & {
   name: string;
+  address?: string;
+  avatarUrl?: string;
+  externalLinks: any[]; // TODO: build out a TS type with name: string, type: string, link: string
+  theme: any[]; // TODO: check for an existing TS type or define one
+  spinampLayoutConfig: any[]; // TODO build out a TS type for type: string, data: string, visible: boolean
   slug: string;
 }
 
@@ -16,11 +21,13 @@ export type ArtistProfile = TimeField & {
   websiteUrl?: string;
 }
 
-export const mapArtist = (artistProfile: ArtistProfile): Artist => {
+export const mapArtist = (artistProfile: ArtistProfile): Partial<Artist> => {
   return {
-    name: artistProfile.name,
-    slug: slugify(`${artistProfile.name} ${artistProfile.createdAtTime.getTime()}`),
     id: artistProfile.artistId,
+    name: artistProfile.name,
+    address: '', // TODO: resolve from earliest profile's artistId, if available
+    avatarUrl: artistProfile.avatarUrl,
+    slug: slugify(`${artistProfile.name} ${artistProfile.createdAtTime.getTime()}`),
     createdAtTime: artistProfile.createdAtTime,
     createdAtEthereumBlockNumber: artistProfile.createdAtEthereumBlockNumber
   }
