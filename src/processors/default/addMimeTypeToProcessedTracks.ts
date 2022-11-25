d98988897c7157865fd9914a51b66d9bc29abac3
puffo <puffo@users.noreply.github.com>
2022-11-25 07:44:07 +0200

    docs: annotate upcoming changes :memo:

--- src/processors/default/addMimeTypeToProcessedTracks.ts
+++ src/processors/default/addMimeTypeToProcessedTracks.ts
@@ -9,6 +9,9 @@ export enum SourceIPFS {
   ARTWORK = 'Artwork'
 }
 
+const TIMEOUT = parseInt(process.env.METADATA_REQUEST_TIMEOUT!)
+
+// TODO: change to take into account nfts, processed tracks, and errors
 export const missingMimeType: (source: SourceIPFS) => Trigger<undefined> = (source) => {
   return async (clients) => {
     const processedTracksQuery = `
@@ -31,11 +34,20 @@ export const addMimeTypeToProcessedTracks: (source: SourceIPFS) => Processor =
     processorFunction: async (processedTracks: ProcessedTrack[], clients: Clients) => {
       console.log(`Fetching processed track ${source} mime types`);
 
+      const metadataErrors: { metadataError: string, nftId: string }[] = [];
+
       const updatedMimeTypes = async (processedTrack: any) => {
         const id = processedTrack.id;
         const ipfsHash = processedTrack[`lossy${source}IPFSHash`];
-        const response = await clients.axios.head(`${process.env.IPFS_ENDPOINT}${ipfsHash}`, { timeout: parseInt(process.env.METADATA_REQUEST_TIMEOUT!) })
+        let response: any;
 
+        try {
+          response = await clients.axios.head(`${process.env.IPFS_ENDPOINT}${ipfsHash}`, { timeout: TIMEOUT })
+        } catch {
+
+        }
+
+        // TODO: error when content type is not MimeEnum
         const result: any = { id: id }
         result[`lossy${source}MimeType`] = response.headers['content-type'];
         return result;
@@ -48,6 +60,7 @@ export const addMimeTypeToProcessedTracks: (source: SourceIPFS) => Processor =
       const updates = results.map(result => result.response)
 
       await clients.db.update(Table.processedTracks, updates);
+      // await clients.db.upsert(Table.nftProcessErrors, metadataErrors, 'nftId', ['metadataError']);
     },
     initialCursor: undefined
   })
