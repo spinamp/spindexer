0e877b0e3dc0cf8cfe5625c0c36af34bc3387e1b
puffo <puffo@users.noreply.github.com>
2022-11-28 14:24:49 +0200

    feat: processedTracks view excludes mime-type-less files :sparkles:

--- src/db/views.ts
+++ src/db/views.ts
@@ -1,18 +1,26 @@
 import { Table } from './db';
 
-// specify override sql to for creating a view
+// specify override sql for creating complex views for particular tables
 export const overrides: {
   [table in Table]?: string;
 } = {
   [Table.processedTracks]: `
-    select t.*
-    from "${Table.processedTracks}" t
+    select t.* from "${Table.processedTracks}" t
+
     join "${Table.ipfsPins}" p
     on t."lossyAudioIPFSHash" = p.id
     join "${Table.ipfsPins}" p1
     on t."lossyArtworkIPFSHash" = p1.id
+
+    join "${Table.ipfsFiles}" f
+    on t."lossyAudioIPFSHash" = f.cid
+    join "${Table.ipfsFiles}" f1
+    on t."lossyArtworkIPFSHash" = f1.cid
+
     where "lossyArtworkIPFSHash" is not null
     and "lossyAudioIPFSHash" is not null
+    and f."mimeType" is not null
+    and f1."mimeType" is not null
     `
 }
 
--- test/db/views.ts
+++ test/db/views.ts
@@ -1,6 +1,7 @@
 import assert from 'assert';
 
 import { DBClient, Table } from '../../src/db/db';
+import { IPFSFile } from '../../src/types/ipfsFile';
 import { ProcessedTrack } from '../../src/types/track';
 import { truncateDB } from '../helpers'
 
@@ -9,13 +10,17 @@ import db from './../../src/db/sql-db';
 describe('views', async () => {
   let dbClient: DBClient;
 
-  // const ipfsFiles: IPFSFile[] = [
-  //   { url: 'https://spinamp.xyz/1xx', cid: '1xx' },
-  //   { url: 'https://spinamp.xyz/2xx', cid: undefined }, // skips undefined
-  //   { url: 'https://spinamp.xyz/3xx', cid: '3xx', mimeType: 'image/jpeg' }, // skips existing
-  //   { url: 'https://spinamp.xyz/4xx', cid: '4xx', error: 'nope' }, // skips errors
-  //   { url: 'https://spinamp.xyz/5xx', cid: '5xx', mimeType: 'image/jpg', error: 'nope' }, // skips with mimeType and errors
-  // ]
+  const ipfsFiles: IPFSFile[] = [
+    { url: 'https://spinamp.xyz/1xx', cid: '1xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/2xx', cid: undefined, mimeType: 'image/jpeg' }, // undefined cid skipped later
+    { url: 'https://spinamp.xyz/3xx', cid: '3xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/4xx', cid: '4xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/5xx', cid: '5xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/6xx', cid: '6xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/7xx', cid: '7xx', mimeType: 'image/jpeg' },
+    { url: 'https://spinamp.xyz/noArtworkMimeType', cid: 'noArtworkMimeType' }, // no mime type skipped later
+    { url: 'https://spinamp.xyz/noAudioMimeType', cid: 'noAudioMimeType' }, // no mime type skipped later
+  ]
 
   const ipfsPins = [
     { id: '1xx', requestId: '1' },
@@ -23,16 +28,23 @@ describe('views', async () => {
     { id: '3xx', requestId: '3' },
     { id: '4xx', requestId: '4' },
     { id: '5xx', requestId: '5' },
+    { id: '6xx', requestId: '6' },
+    { id: '7xx', requestId: '7' },
+    { id: 'noArtworkMimeType', requestId: '8' },
+    { id: 'noAudioMimeType', requestId: '9' },
   ]
 
   const processedTracks = [
     { id: '11', lossyArtworkIPFSHash: '1xx', lossyAudioIPFSHash: '3xx' },
     { id: '22', lossyArtworkIPFSHash: '4xx', lossyAudioIPFSHash: undefined }, // skips undefined
     { id: '33', lossyArtworkIPFSHash: undefined, lossyAudioIPFSHash: '5xx' }, // skips undefined
+    { id: '44', lossyArtworkIPFSHash: 'noArtworkMimeType', lossyAudioIPFSHash: '6xx' }, // skips when missing artwork mimetype
+    { id: '55', lossyArtworkIPFSHash: '7xx', lossyAudioIPFSHash: 'noAudioMimeType' }, // skips when missing audio mimetype
+    { id: '66', lossyArtworkIPFSHash: '1xx', lossyAudioIPFSHash: '2xx' }, // skips undefined cid on ipfsFiles
   ]
 
   const setupFixtures = async () => {
-    // await dbClient.insert<Partial<IPFSFile>>(Table.ipfsFiles, ipfsFiles);
+    await dbClient.insert<Partial<IPFSFile>>(Table.ipfsFiles, ipfsFiles);
     await dbClient.insert(Table.ipfsPins, ipfsPins);
     await dbClient.insert<Partial<ProcessedTrack>>(Table.processedTracks, processedTracks);
   };
@@ -52,7 +64,7 @@ describe('views', async () => {
       const tracks: any = await dbClient.rawSQL('SELECT * FROM processed_tracks');
       const result = tracks.rows;
 
-      assert(result.length === 1, `should only return 1 track based on test data, instead returned ids: ${ result ? result.map((t: any) => t.id) : 'none' }`);
+      assert(result.length === 1, `should only return 1 track based on test data, instead returned ids: ${ result.length > 0 ? result.map((t: any) => t.id) : 'none' }`);
       assert(result[0].id === '11', `incorrect row returned, result was ${JSON.stringify(result[0])}`);
     });
   });
