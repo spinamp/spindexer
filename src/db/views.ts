0e757989d22ce4b8934412ba36352ee91ce82835
puffo <puffo@users.noreply.github.com>
2022-11-29 13:43:55 +0200

    revert: db/views and tests :rewind:

--- src/db/views.ts
+++ src/db/views.ts
@@ -1,26 +1,18 @@
 import { Table } from './db';
 
-// specify override sql for creating complex views for particular tables
+// specify override sql to for creating a view
 export const overrides: {
   [table in Table]?: string;
 } = {
   [Table.processedTracks]: `
-    select t.* from "${Table.processedTracks}" t
-
-    join "${Table.ipfsPins}" audio_pin
-    on t."lossyAudioIPFSHash" = audio_pin.id
-    join "${Table.ipfsPins}" artwork_pin
-    on t."lossyArtworkIPFSHash" = artwork_pin.id
-
-    join "${Table.ipfsFiles}" audio_file
-    on t."lossyAudioIPFSHash" = audio_file.cid
-    join "${Table.ipfsFiles}" artwork_file
-    on t."lossyArtworkIPFSHash" = artwork_file.cid
-
-    where t."lossyArtworkIPFSHash" is not null
-    and t."lossyAudioIPFSHash" is not null
-    and (audio_file."mimeType" is not null and (audio_file."isAudio" = true or audio_file."isVideo" = true))
-    and (artwork_file."mimeType" is not null and (artwork_file."isImage" = true or artwork_file."isVideo" = true))
+    select t.*
+    from "${Table.processedTracks}" t
+    join "${Table.ipfsPins}" p
+    on t."lossyAudioIPFSHash" = p.id
+    join "${Table.ipfsPins}" p1
+    on t."lossyArtworkIPFSHash" = p1.id
+    where "lossyArtworkIPFSHash" is not null
+    and "lossyAudioIPFSHash" is not null
     `
 }
 
