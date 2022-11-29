ac140a663e211a662ab8206d64fbfe46d10fe6c4
Aidan Musnitzky <musnit@gmail.com>
2022-11-29 08:24:09 -0500

    Update src/db/migrations/27-recreate-views.ts

--- src/db/migrations/27-recreate-views.ts
+++ src/db/migrations/27-recreate-views.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex'
 
 
 export const up = async (knex: Knex) => {
-  // old code recreated views
+  // this is deprecated (old code recreated views)
 }
 
 export const down = async (knex: Knex) => {
