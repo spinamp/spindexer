e824bbebc147b303cc4ba49927b2b99b4687c058
puffo <puffo@users.noreply.github.com>
2022-12-01 21:40:44 +0200

    refactor: feedback from PR :ok_hand:

--- src/db/migrations/48-backfill-ipfs-files.ts
+++ src/db/migrations/48-backfill-ipfs-files.ts
@@ -5,17 +5,16 @@ import { Table } from '../db';
 
 export const up = async (knex: Knex) => {
   const tracksWithCIDsMatchingURL = await knex.raw(`
-    select distinct "lossyArtworkIPFSHash", "lossyArtworkURL" from "${Table.processedTracks}" as t
-    left outer join "${Table.ipfsFiles}" as i
-    on t."lossyArtworkURL" = i.url
-    where (
-      (t."lossyArtworkIPFSHash" is not null and t."lossyArtworkURL" is not null)
-      and
-      (t."lossyArtworkURL" ILIKE ('%' || t."lossyArtworkIPFSHash" || '%'))
-      and
-      (i.url is null and i.error is null)
-    )
+  select distinct "lossyArtworkIPFSHash" from "${Table.processedTracks}" as t
+  left outer join "${Table.ipfsFiles}" as i
+  on t."lossyArtworkIPFSHash" = i.cid
+  where
+    t."lossyArtworkIPFSHash" is not null and
+    i.cid is null and
+    i.error is null and
+    t."lossyArtworkURL" ILIKE ('%' || t."lossyArtworkIPFSHash" || '%')
   `)
+
   const ipfsFiles = tracksWithCIDsMatchingURL.rows.map((row: any) => ({
     url: row.lossyArtworkURL,
     cid: row.lossyArtworkIPFSHash,
