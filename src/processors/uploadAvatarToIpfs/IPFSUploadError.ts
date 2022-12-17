0e928e8bc2ec0ef0a78cea6170eac8a6924fdc27
Max Tyrrell <m@xtyrrell.com>
2022-12-17 15:01:34 +0200

    refactor: reintroduce and use getErrorMessage

--- src/processors/uploadAvatarToIpfs/IPFSUploadError.ts
+++ src/processors/uploadAvatarToIpfs/IPFSUploadError.ts
@@ -1,13 +1,17 @@
 export class IPFSUploadError extends Error {
+  readonly name = 'IPFSUploadError';
   readonly url: string;
 
   constructor(message: string, url: string) {
-    super(message);
+    super(getErrorMessage(message, url));
 
     // 👇️ because we are extending a built-in class
     Object.setPrototypeOf(this, IPFSUploadError.prototype);
 
     this.url = url;
   }
+}
 
+function getErrorMessage(url: string, message: string) {
+  return `Error when uploading file to IPFS at url "${url}": ${message}`;
 }
